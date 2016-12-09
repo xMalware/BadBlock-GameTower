2cdf571584c3411dee8b4374fa95144aa73ff95b
xMalware <xmalware2@gmail.com>
2016-12-09 13:03:15 +0100

    Fix Tower death

--- src/fr/badblock/tower/listeners/DeathListener.java
+++ src/fr/badblock/tower/listeners/DeathListener.java
@@ -54,6 +54,8 @@ public class DeathListener extends BadListener {
 	private void death(FakeDeathEvent e, BadblockPlayer player, Entity killer, DamageCause last){
 		if(player.getTeam() == null) return; //WTF
 
+		if (player.getOpenInventory() != null && player.getOpenInventory().getCursor() != null)
+			player.getOpenInventory().setCursor(null);
 		Location respawnPlace = null;
 
 		player.getPlayerData().incrementStatistic("tower", TowerScoreboard.DEATHS);
