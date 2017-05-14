83fb898653cb7eafd254fd5b46e5eb86aedb924c
xMalware <xmalware2@gmail.com>
2017-05-14 10:04:02 +0200

    fix

--- src/fr/badblock/bukkit/games/tower/runnables/StartRunnable.java
+++ src/fr/badblock/bukkit/games/tower/runnables/StartRunnable.java
@@ -129,6 +129,7 @@ public class StartRunnable extends BukkitRunnable {
 		} else if(task != null){
 			task.cancel();
 			time = TIME_BEFORE_START;
+			GameAPI.setJoinable(true);
 		}
 		
 		task = null;
