b7a2c7bcaf5e24c16e89703137bc49031dcdf43f
xMalware <xmalware2@gmail.com>
2017-05-14 09:29:13 +0200

    fix

--- src/fr/badblock/bukkit/games/tower/runnables/StartRunnable.java
+++ src/fr/badblock/bukkit/games/tower/runnables/StartRunnable.java
@@ -105,14 +105,14 @@ public class StartRunnable extends BukkitRunnable {
 	}
 
 	public static void joinNotify(int currentPlayers, int maxPlayers){
+		if(currentPlayers < PluginTower.getInstance().getConfiguration().minPlayers) return;
+		
+		startGame();
 		if (task != null) {
 			int a = time - (TIME_BEFORE_START / Bukkit.getMaxPlayers());
 			if (time >= 60 && (a <= 60 || Bukkit.getOnlinePlayers().size() >= Bukkit.getMaxPlayers())) time = 60;
 			else if (time >= 60) time = a;
 		}
-		if(currentPlayers < PluginTower.getInstance().getConfiguration().minPlayers) return;
-		
-		startGame();
 	}
 	
 	public static void startGame(){
